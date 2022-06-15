90f907ba9ea1f38e7b11494c4d9c3b2e342e8bb7
YuriiMololodtsov <88619986+YuriiMololodtsov@users.noreply.github.com>
2022-06-15 14:28:14 +0300

    added application logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import '../src/components/card.css';
 import './app.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid4 } from 'uuid';
 
 function App() {
+  const [item, setItem] = useState(); //слово на англ.
+  const [item2, setItem2] = useState(); //перевод
+
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || [] //парсим хранилище
+  );
+
+  //записываем массив в хранилище
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
+  //создаем новый елемент, добавляем в массив, чистим импуты
+  const newItem = () => {
+    if (item.trim() !== '') {
+      const newItem = {
+        id: uuid4(),
+        item: item,
+        item2: item2,
+      };
+      setItems((item) => [...items, newItem]);
+      setItem('');
+      setItem2('');
+    } else {
+      alert('Введите слово, и перевод...');
+      setItem('');
+      setItem2('');
+    }
+  };
+
+  //кнопка удаления карточки
+  const deleteNode = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="App">
       <header className="header">
@@ -17,68 +52,47 @@ function App() {
       <main>
         <div className="form">
           <input
+            value={item}
             className="form__word"
             placeholder="Введите слово на англ..."
+            onChange={(e) => setItem(e.target.value)}
           ></input>
 
           <input
+            value={item2}
             className="form__translat"
             placeholder="Введите перевод..."
+            onChange={(e) => setItem2(e.target.value)}
           ></input>
 
-          <button className="form__btn">Создать</button>
-        </div>
-        {/* <div className="cards">
-        <div className="card center">
-          <div className="card__front">
-            <span>FRONT</span>
-          </div>
-          <div class="cards__back">
-            <span class="card__text center">back</span>
-          </div>
+          <button onClick={newItem} className="form__btn">
+            Создать
+          </button>
         </div>
 
-          <div className="cards__card">
-          <div className="cards__title">DOG</div>
-        </div>
-        <div className="cards__card">
-          <div className="cards__title">BIRD</div>
-        </div>
-        <div className="cards__card">
-          <div className="cards__title">COW</div>
-        </div>
-        <div className="cards__card">
-          <div className="cards__title">HORSE</div>
-        </div> 
-      </div> */}
         <div className="cards">
-          <div class="card center">
-            <div class="front">CAT</div>
+          {items.map((item) => {
+            <p>{`${item.item}`}</p>;
+          })}
+          {items.map((item, index) => {
+            return (
+              <div class="card center">
+                <div class="front">{item.item}</div>
 
-            <div class="back">
-              <div class="back-content center">
-                <span>КОТ</span>
+                <div class="back">
+                  <div class="back-content center">
+                    <span>{item.item2}</span>
+                  </div>
+                  <button
+                    className="delete"
+                    onClick={() => deleteNode(item.id)}
+                  >
+                    X
+                  </button>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="card center">
-            <div class="front">DOG</div>
-
-            <div class="back">
-              <div class="back-content center">
-                <span>ПЁС</span>
-              </div>
-            </div>
-          </div>
-          <div class="card center">
-            <div class="front">FISH</div>
-
-            <div class="back">
-              <div class="back-content center">
-                <span>РЫБА</span>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </main>
       <footer className="footer"></footer>
